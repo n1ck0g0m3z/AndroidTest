51b282f04e37fd39d6b52ceeb7534e1483a8a3ec
student <student>
2016-05-21 18:17:24 +0900

    UI

--- app/src/main/java/com/example/teacher/test/IntentActivity.java
+++ app/src/main/java/com/example/teacher/test/IntentActivity.java
@@ -4,11 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.example.teacher.test.util.UIActivity;
 
 import butterknife.OnClick;
 
--- app/src/main/java/com/example/teacher/test/UIActivity.java
+++ app/src/main/java/com/example/teacher/test/UIActivity.java
@@ -1,4 +1,4 @@
-package com.example.teacher.test.util;
+package com.example.teacher.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
